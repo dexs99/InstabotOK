d75d1fbf27ac4b314fee12e467e07cc72a730eae
dexs99 <38705585+dexs99@users.noreply.github.com>
2018-05-12 09:12:33 +0200

    Add files via upload

---
+++ src/check_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from .user_info import get_user_info
+
+
+def check_status(self):
+    self.is_self_checking = True
+    self.is_checked = False
+    #while self.is_checked != True: <-- original
+    while self.is_checked is not True:
+        get_user_info(self, self.user_login)
+    self.like_counter = 0
+    self.follow_counter = 0
+    self.unfollow_counter = 0
